5c3cf7ef11e14a094aaeef7af5194c3cc4c423ca
William Chan <william.chan@gearboxsf.com>
2023-04-30 18:05:06 -0700

    Update jest configs and add initial tests

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("Renders button without crashing", () => {
+    render(<Home />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("Renders button correctly", () => {
+    render(<Home />);
+    expect(screen.getByRole("button")).toHaveTextContent("Go to Spotify App");
+  });
+});
